a98a5d34c2b4736bd58f55ebbb7534e4b33e57f9
asmeziou <asmeziou@gmail.com>
2026-06-18 14:18:44 +0200

    first commit

---
+++ app/index.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { TextInput, Text, View, Image, StyleSheet } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+const HomePage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errormessage, setErrormessage] = useState("");
+
+  return (
+    <KeyboardAwareScrollView style={styles.container}>
+      <View style={styles.titreandlogo}>
+        <Text style={styles.titre}>Sign Up</Text>
+        <View>
+          <Image
+            source={require("../assets/logo.png")}
+            style={styles.cover}
+            resizeMode="contain"
+          ></Image>
+        </View>
+      </View>
+      <View>
+        <TextInput
+          style={styles.inputs}
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+          value={email}
+          placeholder="Email"
+        />
+
+        <TextInput
+          style={styles.inputs}
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+          value={password}
+        />
+      </View>
+    </KeyboardAwareScrollView>
+  );
+};
+
+export default HomePage;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+  },
+  titreandlogo: {
+    flex: 1,
+    alignItems: "center",
+  },
+  titre: {
+    color: "#F75050",
+    fontSize: 40,
+    marginTop: 100,
+  },
+  cover: {
+    width: 200,
+    height: 200,
+  },
+  inputs: {
+    height: 44,
+    borderBottomWidth: 1,
+    borderColor: "#F75050",
+    marginBottom: 10,
+  },
+});
